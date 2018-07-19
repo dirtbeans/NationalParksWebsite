ef5254169d6dfafc6d923e51e930e475ef41fe17
Dustin Mattox <barch54@gmail.com>
2018-07-19 14:52:36 -0400

    added integration test

---
+++ m3-w3d3-csharp-capstone/Capstone.Web.Tests/DAL/NationalParkSqlDalTests.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Transactions;
+using System.Data.SqlClient;
+using System.Configuration;
+using Capstone.Web.DAL;
+using System.Collections.Generic;
+using Capstone.Web.Models;
+
+namespace Capstone.Web.Tests.DAL
+{
+    [TestClass()]
+    public class NationalParkSqlDalTests
+    {
+        private TransactionScope tran;
+        private string connectionString = ConfigurationManager.ConnectionStrings["NPGeek"].ConnectionString;
+        private string parkCode;
+        private int parkCount = 0;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            tran = new TransactionScope();
+
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                SqlCommand cmd;
+
+                conn.Open();
+
+                cmd = new SqlCommand("INSERT INTO park VALUES ('XXX', 'Test Park', 'Ohio', 65536, 5280, 1337, 99, 'Forest', 2018, 50, 'To test or not to test', 'Andrew Frank', 'A test park located in the forest', 19, 100);", conn);
+                cmd.ExecuteNonQuery();
+
+                cmd = new SqlCommand("INSERT INTO weather VALUES ('XXX', 1, 40, 70, 'rain');", conn);
+                cmd.ExecuteNonQuery();
+
+                parkCode = "XXX";
+
+                cmd = new SqlCommand("SELECT * FROM park;", conn);
+                SqlDataReader reader = cmd.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    parkCount++;
+                }
+                                
+            }
+        }
+
+        [TestCleanup]
+        public void Cleanup()
+        {
+            tran.Dispose();
+        }
+
+
+        [TestMethod()]
+        public void GetAllParksTest()
+        {
+
+            // Arrange 
+            NationalParkSqlDal parkDal = new NationalParkSqlDal(connectionString);
+
+            //Act
+            List<Park> parks = parkDal.GetAllParks();
+
+            //Assert
+            Assert.AreEqual(parks.Count, parkCount);
+
+        }
+
+        [TestMethod()]
+        public void GetOneParkTest()
+        {
+
+            // Arrange 
+            NationalParkSqlDal parkDal = new NationalParkSqlDal(connectionString);
+
+            //Act
+            Park park = parkDal.GetOnePark("XXX");
+
+            //Assert
+            Assert.AreEqual(parkCode, park.ParkCode);
+
+        }
+    }
+}
--- m3-w3d3-csharp-capstone/Capstone.Web/DAL/WeatherSqlDal.cs
+++ m3-w3d3-csharp-capstone/Capstone.Web/DAL/WeatherSqlDal.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Capstone.Web.Models;
+using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 
@@ -7,5 +9,48 @@ namespace Capstone.Web.DAL
 {
     public class WeatherSqlDal
     {
+        private readonly string connectionString;
+        private const string SQL_GetWeatherForPark = "SELECT * FROM weather WHERE parkCode = @parkCode";
+
+        public WeatherSqlDal(string connectionString)
+        {
+            this.connectionString = connectionString;
+        }
+
+        public Weather GetWeatherForPark(string parkCode)
+        {
+            Weather weather = new Weather();
+
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    conn.Open();
+
+                    SqlCommand cmd = new SqlCommand(SQL_GetWeatherForPark, conn);
+
+                    cmd.Parameters.AddWithValue("@parkCode", parkCode);
+
+                    SqlDataReader reader = cmd.ExecuteReader();
+
+                    while (reader.Read())
+                    {
+                        weather.ParkCode = Convert.ToString(reader["parkCode"]);
+                        weather.FiveDayForecastValue = Convert.ToInt32(reader["fiveDayForecastValue"]);
+                        weather.Low = Convert.ToInt32(reader["low"]);
+                        weather.High = Convert.ToInt32(reader["high"]);
+                        weather.Forecast = Convert.ToString(reader["forecast"]);
+                    }
+
+                }
+            }
+            catch (SqlException ex)
+            {
+                throw;
+            }
+
+            return weather;
+        }
+
     }
 }
